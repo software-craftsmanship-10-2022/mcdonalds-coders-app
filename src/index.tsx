85f2be8f2e2c12dffbc24efcddbfab4ac11689e0
David Casado <dcasadomartinez@gmail.com>
2022-11-05 12:42:23 +0100

    Ship: Fix bug that prevented build

--- src/index.tsx
+++ src/index.tsx
@@ -19,8 +19,10 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-try {
-  await reportWebVitals();
-} catch (error: unknown) {
-  console.error((error as ImportError).message); 
-}
+(async function() {
+  try {
+    await reportWebVitals();
+  } catch (error: unknown) {
+    console.error((error as ImportError).message); 
+  }
+})()

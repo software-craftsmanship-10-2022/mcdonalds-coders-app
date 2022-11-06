1acf45f41af6a20e65c72678d97d300a3fa50321
Dani Cruz <danielcru@gmail.com>
2022-11-06 23:55:43 +0100

    create errorMessages constants

---
+++ src/errorMessages.ts
@@ -0,0 +1,18 @@
+export const CARD_ERRORS = {
+  WRONG_CARD_NUMBER: "wrong card number",
+  WRONG_DATE: "wrong date",
+  WRONG_CVC: "wrong CVC",
+};
+
+export const DONATION_ERRORS = {
+  POSITIVE_NUMBER: "Amount must be a positive number",
+};
+
+export const ORDER_ERRORS = {
+  OVER_0_NUMBER: "Order amount must be greater than 0",
+};
+
+export const ACCOUNT_ERRORS = {
+  FULL_NAME_FORMAT: "Full name needs first and last name",
+  SWIFT_VALIDATION: "Swift is not valid",
+};

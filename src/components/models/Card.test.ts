b16a3983bb0474cf39fc8f7315106083e1afb7c1
ivdobry <ivandobry@hotmail.es>
2022-11-06 10:52:08 +0100

    add cvc validation

--- src/components/models/Card.test.ts
+++ src/components/models/Card.test.ts
@@ -13,12 +13,18 @@ describe("Given a Card class", () => {
     it("Should throuw an error with length is not valid", () => {
       const card = new Card("231123132", "12/23", 123)
 
-      expect(() => card.isValid()).toThrowError("wrong cardNumber")
+      expect(() => card.isValid()).toThrowError("wrong card number")
     })
 
     it("Should throuw an error when date is not valid", () => {
       const card = new Card("1299999999999999", "13/23", 123)
 
       expect(() => card.isValid()).toThrowError("wrong date")
+    })
+
+    it("Should throuw an error when cvc is not valid", () => {
+      const card = new Card("1299999999999999", "12/23", 1223)
+
+      expect(() => card.isValid()).toThrowError("wrong CVC")
     })
 })
--- src/components/models/Card.ts
+++ src/components/models/Card.ts
@@ -1,6 +1,8 @@
-import IValidate from "./Ivalidate";
+import IValidate from "./IValidate";
 
-const ERROR_CARD_NUMBER = "wrong cardNumber"
+const ERROR_CARD_NUMBER = "wrong card number"
+const ERROR_CARD_DATE = "wrong date"
+const ERROR_CARD_CVC = "wrong CVC"
 
 class Card implements IValidate {
     cardNumber
@@ -16,7 +18,8 @@ class Card implements IValidate {
     isValid(): boolean {
         let dateRegEx = /^(0[1-9]|1[0-2])\/(0[1-9]|1[1-9]|2[1-9])$/;
         if(this.cardNumber.length !== 16) throw new Error(ERROR_CARD_NUMBER)
-        if(!dateRegEx.test(this?.date)) throw new Error("wrong date")
+        if(!dateRegEx.test(this?.date)) throw new Error(ERROR_CARD_DATE)
+        if(this.cvc.toString().length !== 3) throw new Error(ERROR_CARD_CVC)
         
         return true
     }

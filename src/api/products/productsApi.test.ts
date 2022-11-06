e8f2f742022770d712d4aee05b067e646ae31fef
jocalu <jocalu87@gmail.com>
2022-11-06 17:31:47 +0100

    feature#5 Remove unusuless jest utils in productApi test

--- src/api/products/productsApi.test.ts
+++ src/api/products/productsApi.test.ts
@@ -1,32 +1,25 @@
+// GIVEN getAllProducts function WHEN you call the function THEN
+// must be a function
+// Must return a promise
+// Must return an array of products after the promise is resolved
 
-// GIVEN getAllProducts function WHEN you call the function THEN 
- // must be a function
- // Must return a promise
- // Must return an array of products after the promise is resolved
+import PRODUCTS from 'src/data/products';
+import {getAllProducts} from './productsApi';
 
-import PRODUCTS from "src/data/products";
-import { getAllProducts } from "./productsApi";
-
-describe('GIVEN getAllProducts function WHEN you call the function THEN', ()=>{
-  afterEach(() => {
-    jest.clearAllMocks();
-    jest.useRealTimers();
-    jest.clearAllTimers();
+describe('GIVEN getAllProducts function WHEN you call the function THEN', () => {
+  test('must be a function', () => {
+    expect(typeof getAllProducts).toBe('function');
   });
 
-  test('must be a function', ()=>{
-    expect(typeof getAllProducts).toBe("function");
-  });
-  
-  test('Must return all products data after the promise is resolved', async ()=>{
+  test('must return all products data after the promise is resolved', async () => {
     await expect(getAllProducts()).resolves.toEqual(PRODUCTS);
   });
-  
-  test('Must resolve the promise in 100ms', async ()=>{
-    jest.useFakeTimers();
-    jest.spyOn(global, 'setTimeout');
-    getAllProducts()
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 100);
+
+  test('must resolve the promise in 100ms', async () => {
+    const setTimeoutSpy = jest.spyOn(global, 'setTimeout');
+
+    await getAllProducts();
+    expect(setTimeoutSpy).toHaveBeenCalledTimes(1);
+    expect(setTimeoutSpy).toHaveBeenLastCalledWith(expect.any(Function), 100);
   });
 });

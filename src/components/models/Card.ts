6b59d8cc4b8fa5a234912a407ad0692b213e2cd9
Dani Cruz <danielcru@gmail.com>
2022-11-06 23:56:54 +0100

    refactor card class & test with new constants

--- src/components/models/Card.ts
+++ src/components/models/Card.ts
@@ -1,28 +1,28 @@
+import { CARD_ERRORS } from "../../errorMessages";
 import IValidate from "./IValidate";
 
-const ERROR_CARD_NUMBER = "wrong card number"
-const ERROR_CARD_DATE = "wrong date"
-const ERROR_CARD_CVC = "wrong CVC"
-const REGEX_DATE = new RegExp("^(0[1-9]|1[0-2])\/(0[1-9]|1[1-9]|2[1-9])$")
+const REGEX_DATE = new RegExp("^(0[1-9]|1[0-2])/(0[1-9]|1[1-9]|2[1-9])$");
 
 class Card implements IValidate {
-    cardNumber
-    date
-    cvc
-    
-    constructor(cardNumber: string, date: string, cvc: number) {
-        this.cardNumber = cardNumber
-        this.date = date
-        this.cvc = cvc
-    }
+  cardNumber;
+  date;
+  cvc;
 
-    isValid(): boolean {
-        if(this.cardNumber.length !== 16) throw new Error(ERROR_CARD_NUMBER)
-        if(!REGEX_DATE.test(this?.date)) throw new Error(ERROR_CARD_DATE)
-        if(this.cvc.toString().length !== 3) throw new Error(ERROR_CARD_CVC)
-        
-        return true
-    }
+  constructor(cardNumber: string, date: string, cvc: number) {
+    this.cardNumber = cardNumber;
+    this.date = date;
+    this.cvc = cvc;
+  }
+
+  isValid(): boolean {
+    if (this.cardNumber.length !== 16)
+      throw new Error(CARD_ERRORS.WRONG_CARD_NUMBER);
+    if (!REGEX_DATE.test(this?.date)) throw new Error(CARD_ERRORS.WRONG_DATE);
+    if (this.cvc.toString().length !== 3)
+      throw new Error(CARD_ERRORS.WRONG_CVC);
+
+    return true;
+  }
 }
 
-export default Card
+export default Card;

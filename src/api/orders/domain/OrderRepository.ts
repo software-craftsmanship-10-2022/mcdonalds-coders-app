35faa3839ef4ca3eb9d21919e703e3483638ca43
Alex Park <a.park@tipsterchat.com>
2022-11-09 21:15:36 +0100

    orders: update repositories imports

--- src/api/orders/domain/OrderRepository.ts
+++ src/api/orders/domain/OrderRepository.ts
@@ -1,4 +1,4 @@
-import {OrderStatus} from 'src/@types/order.d';
+import type {OrderStatus} from 'src/@types/order.d';
 
 export type OrderRepository = {
   save: (orderId: string, status: OrderStatus) => Promise<void>;

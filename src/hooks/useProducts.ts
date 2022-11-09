293467b189bbfe7a72b54d3ed22ffac1b27b2395
jocalu <jocalu87@gmail.com>
2022-11-09 12:34:45 +0100

    useProducts works without useEffect

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,20 +1,18 @@
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import type {ProductCategoryType} from '~types/product';
 import {findProductsByCategoryId, getAllProducts} from '../api/products/productsApi';
 
 export const useProducts = () => {
   const [products, setProducts] = useState<ProductCategoryType[]>([]);
 
-  useEffect(() => {
-    const fetchAllProducts = async (): Promise<void> => {
-      const response = await getAllProducts();
-      setProducts(response);
-    };
+  const fetchAllProducts = async (): Promise<void> => {
+    const response = await getAllProducts();
+    setProducts(response);
+  };
 
-    fetchAllProducts().catch((error) => {
-      console.error(error);
-    });
-  }, [products]);
+  fetchAllProducts().catch((error) => {
+    console.error(error);
+  });
 
   return {
     products,
@@ -28,16 +26,14 @@ export const useProductsByCategory = (categoryId: string | undefined) => {
     items: [],
   });
 
-  useEffect(() => {
-    const fetchProductsByCategory = async (categoryId: string | undefined): Promise<void> => {
-      const response = await findProductsByCategoryId(categoryId);
-      setProductsByCategory(response);
-    };
+  const fetchProductsByCategory = async (categoryId: string | undefined): Promise<void> => {
+    const response = await findProductsByCategoryId(categoryId);
+    setProductsByCategory(response);
+  };
 
-    fetchProductsByCategory(categoryId).catch((error) => {
-      console.error(error);
-    });
-  }, [productsByCategory]);
+  fetchProductsByCategory(categoryId).catch((error) => {
+    console.error(error);
+  });
 
   return {
     productsByCategory,

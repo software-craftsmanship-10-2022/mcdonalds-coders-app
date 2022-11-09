10bc04c9039285feb31cd2efd0789877aa382377
Ivan <83778683+ivdobry@users.noreply.github.com>
2022-11-09 15:18:11 +0100

    Update src/Payment/models/Card/Card.test.ts
    Co-authored-by: Jaume Villarreal <jaumevillarreal@gmail.com>

--- src/Payment/models/Card/Card.test.ts
+++ src/Payment/models/Card/Card.test.ts
@@ -19,7 +19,7 @@ describe('Given a Card class', () => {
     expect(() => card.isValid()).toThrowError(CARD_ERRORS.wrongDate);
   });
 
-  it('Should throw an error when cvc is not valid', () => {
+  it('when cvc is not valid then an error should be thrown', () => {
     const card = new Card('1299999999999999', '12/23', 1223);
 
     expect(() => card.isValid()).toThrowError(CARD_ERRORS.wrongCvc);

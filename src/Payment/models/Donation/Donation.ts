309ba1d7bd4806eeaf7758d914927748037e78a1
Dani Cruz <danielcru@gmail.com>
2022-11-11 17:24:07 +0100

    test: refactor donation tests

--- src/Payment/models/Donation/Donation.ts
+++ src/Payment/models/Donation/Donation.ts
@@ -4,6 +4,9 @@ class Donation {
   constructor(private readonly amount: number) {}
 
   checkAmountValue() {
+    if (this.amount === undefined || this.amount === null)
+      throw new Error(DONATION_ERRORS.noDonationValue);
+    if (typeof this.amount !== 'number') throw new Error(DONATION_ERRORS.donationAsNumber);
     if (this.amount < 0) throw new Error(DONATION_ERRORS.positiveNumber);
   }
 

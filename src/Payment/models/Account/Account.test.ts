b81d7cad582976952a3a2b4cf9ade46d543e3d64
Dani Cruz <danielcru@gmail.com>
2022-11-11 17:21:37 +0100

    test: refactor Account tests

--- src/Payment/models/Account/Account.test.ts
+++ src/Payment/models/Account/Account.test.ts
@@ -6,11 +6,25 @@ describe('Given an Account class', () => {
     const account = new Account('john doe', 'SECTARB1XXX');
     expect(account).toBeInstanceOf(Object);
   });
+  it('when fullname is not setted then an error should be thrown', () => {
+    // @ts-expect-error desactivamos ts para forzar el test
+    const account = new Account(undefined, 'SECTARB1XXX');
+    expect(() => account.isValid()).toThrowError(ACCOUNT_ERRORS.fullNameFormat);
+  });
+  it('when fullname is not string then an error should be thrown', () => {
+    // @ts-expect-error desactivamos ts para forzar el test
+    const account = new Account(4342, 'SECTARB1XXX');
+    expect(() => account.isValid()).toThrowError(ACCOUNT_ERRORS.fullNameFormat);
+  });
   it('when fullname is not valid then an error should be thrown', () => {
     const account = new Account('John', 'SECTARB1XXX');
     expect(() => account.isValid()).toThrowError(ACCOUNT_ERRORS.fullNameFormat);
   });
-
+  it('when IBAN number is not setted then an error should be thrown', () => {
+    // @ts-expect-error desactivamos ts para forzar el test
+    const account = new Account('john doe', undefined);
+    expect(() => account.isValid()).toThrowError(ACCOUNT_ERRORS.ibanFormat);
+  });
   it('when IBAN number is not valid then an error should be thrown', () => {
     const account = new Account('john doe', 'SCTARB1XXX');
     expect(() => account.isValid()).toThrowError(ACCOUNT_ERRORS.ibanFormat);

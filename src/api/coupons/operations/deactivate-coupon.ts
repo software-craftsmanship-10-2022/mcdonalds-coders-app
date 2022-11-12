25100dc5faf6fcfbbca60c1e23a559ca45369260
sasrous <n.casascrous@gmail.com>
2022-11-12 20:47:33 +0100

    fix ts issues in DDBB functions

--- src/api/coupons/operations/deactivate-coupon.ts
+++ src/api/coupons/operations/deactivate-coupon.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import type {CouponType} from 'src/@types/coupon';
 import {STORAGE} from 'src/config';
 import {getFromDDBB, saveInDDBB} from '../shared/couponsDDBBFunctions';
@@ -6,15 +5,25 @@ import {getFromDDBB, saveInDDBB} from '../shared/couponsDDBBFunctions';
 async function deactivateCoupon(id: string): Promise<void> {
   if (!id || !id.length) throw new TypeError('Item id is not defined');
 
-  const activeCoupons: CouponType[] = getFromDDBB(STORAGE.activeCoupons);
-  const inactiveCoupons: CouponType[] = getFromDDBB(STORAGE.inactiveCoupons);
-  const selectedCoupon: CouponType = activeCoupons.find((coupon: CouponType) => coupon.id === id)!;
+  const activeCoupons = getFromDDBB<CouponType[]>(STORAGE.activeCoupons);
+  const inactiveCoupons = getFromDDBB<CouponType[]>(STORAGE.inactiveCoupons);
+  if (!activeCoupons) {
+    throw new Error('Item is not active');
+  }
+
+  const selectedCoupon: CouponType | undefined = activeCoupons.find(
+    (coupon: CouponType) => coupon.id === id,
+  );
+  if (!selectedCoupon) {
+    throw new Error('Item is not active');
+  }
+
   const updatedActiveCoupons = activeCoupons.filter((coupon: CouponType) => coupon.id !== id);
   const updatedInactiveCoupons = inactiveCoupons
     ? [...inactiveCoupons, selectedCoupon]
     : [selectedCoupon];
-  saveInDDBB(STORAGE.activeCoupons, updatedActiveCoupons);
-  saveInDDBB(STORAGE.inactiveCoupons, updatedInactiveCoupons);
+  saveInDDBB<CouponType[]>(STORAGE.activeCoupons, updatedActiveCoupons);
+  saveInDDBB<CouponType[]>(STORAGE.inactiveCoupons, updatedInactiveCoupons);
 }
 
 export default deactivateCoupon;

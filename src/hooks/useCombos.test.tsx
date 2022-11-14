16c7bbb76e92de8936f78590a790c6a325fb1525
jmora-dev <dam2mora@gmail.com>
2022-11-14 11:15:01 +0100

    Add useCombos hook

---
+++ src/hooks/useCombos.test.tsx
@@ -0,0 +1,67 @@
+import {fireEvent, render, screen} from '@testing-library/react';
+import {useEffect, useState} from 'react';
+import type {ComboDetailType} from 'src/@types/combos';
+import COMBOS from 'src/data/combos';
+import useCombos from './useCombos';
+
+const CATEGORIES_LENGTH = 'Categories: ';
+const COMBO_SEARCHED = 'Combo searched: ';
+const NOT_SELECTED = 'not selected';
+const COMBO_SELECT_BUTTON = 'SELECT COMBO';
+const TestComponent = () => {
+  const {combos, getAllCombos, getComboById} = useCombos();
+  const [selected, setSelected] = useState<ComboDetailType | undefined>(undefined);
+
+  useEffect(() => {
+    getAllCombos();
+  }, []);
+
+  const onClickSelectCombo = () => {
+    getComboById(COMBOS[3].items[1].id)
+      .then((result) => {
+        setSelected(result);
+      })
+      .catch(console.error);
+  };
+
+  return (
+    <div>
+      <span>
+        {CATEGORIES_LENGTH}
+        {combos.length}
+      </span>
+      <button onClick={onClickSelectCombo}>{COMBO_SELECT_BUTTON}</button>
+      <span>
+        {COMBO_SEARCHED}
+        {selected?.title ?? NOT_SELECTED}
+      </span>
+    </div>
+  );
+};
+
+describe('Given useCombos hook', () => {
+  test('TestComponent should render', () => {
+    render(<TestComponent />);
+  });
+
+  test('when render the component then combos should be reloaded', async () => {
+    render(<TestComponent />);
+    await screen.findAllByText(CATEGORIES_LENGTH + COMBOS.length.toString());
+  });
+
+  test('when render the component then combos should be reloaded', async () => {
+    render(<TestComponent />);
+    await screen.findAllByText(CATEGORIES_LENGTH + COMBOS.length.toString());
+  });
+
+  test('when press select combo button then should search the data of fourth category', async () => {
+    render(<TestComponent />);
+
+    screen.getByText(COMBO_SEARCHED + NOT_SELECTED);
+
+    const button = screen.getByText(COMBO_SELECT_BUTTON);
+    fireEvent.click(button);
+
+    await screen.findAllByText(COMBO_SEARCHED + COMBOS[3].items[1].title);
+  });
+});
---
+++ src/hooks/useCombos.ts
@@ -0,0 +1,32 @@
+import {useState} from 'react';
+import type {ComboCategoryType, ComboDetailType} from 'src/@types/combos';
+import {getAllCombosFromApi, getComboDetailByIdFromApi} from '../api/products/combosApi';
+import {getSessionStorageItem, setSessionStorageItem} from './useSessionStorage';
+
+const useCombos = () => {
+  const [combos, setCombos] = useState<ComboCategoryType[]>([]);
+
+  const getAllCombos = (): void => {
+    const combosFromCache = getSessionStorageItem<ComboCategoryType[] | undefined>('combos');
+
+    if (combosFromCache) {
+      setCombos(combosFromCache);
+      return;
+    }
+
+    getAllCombosFromApi()
+      .then((combosFromApi) => {
+        setCombos(combosFromApi);
+        setSessionStorageItem('combos', combosFromApi);
+      })
+      .catch(console.error);
+  };
+
+  const getComboById = async (comboId: string): Promise<ComboDetailType> => {
+    return getComboDetailByIdFromApi(comboId);
+  };
+
+  return {combos, getAllCombos, getComboById};
+};
+
+export default useCombos;

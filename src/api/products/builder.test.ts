5208816f8bdad1c7e6d5e791248a6f452fc05a8a
David Casado <dcasadomartinez@gmail.com>
2022-11-19 16:26:21 +0100

    Create the `withDrink` method

--- src/api/products/builder.test.ts
+++ src/api/products/builder.test.ts
@@ -1,48 +1,194 @@
-import {MenuBuilder} from './builder';
+import type {MenuType, ProductType} from 'src/@types/product';
+import {ERRORS, MenuBuilder} from './builder';
+
+const menus: Record<string, MenuType> = {
+  kj7Stiwpn5: {
+    id: 'kj7Stiwpn5',
+    image: 'McCOMBOBIGMACGrande.png',
+    name: 'McCombo BIG MAC Grande',
+    price: 990,
+    products: [
+      {
+        categoryId: 'burgers',
+        description: 'La hamburguesa más famosa del mundo. Un sabor único.',
+        id: 'big_mac',
+        img: 'big_mac.png',
+        ingredients: [
+          {
+            extraPrice: 0,
+            id: 'pan-arriba',
+            img: 'Pan+arriba.png',
+            modifaible: false,
+            title: 'Pan',
+          },
+          {extraPrice: 0, id: 'pan-abajo', img: 'Pan+abajo.png', modifaible: false, title: 'Pan'},
+          {extraPrice: 0, id: 'carne', img: 'carne.png', modifaible: false, title: 'Carne'},
+          {
+            extraPrice: 0,
+            id: 'salsa-bigmac',
+            img: 'salsa-bic-mac.png',
+            modifaible: true,
+            title: 'Salsa Big Mac',
+          },
+        ],
+        title: 'Big Mac',
+      },
+    ],
+  },
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  '4IkmjbhAKy': {
+    id: '4IkmjbhAKy',
+    image: 'McCOMBOBIGMACMediano.png',
+    name: 'McCombo BIG MAC Mediano',
+    price: 870,
+    products: [
+      {
+        categoryId: 'burgers',
+        description: 'La hamburguesa más famosa del mundo. Un sabor único.',
+        id: 'big_mac',
+        img: 'big_mac.png',
+        ingredients: [
+          {extraPrice: 0, id: 'pan-arriba', img: 'Pan+arriba.png', modifaible: false, title: 'Pan'},
+          {extraPrice: 0, id: 'pan-abajo', img: 'Pan+abajo.png', modifaible: false, title: 'Pan'},
+          {extraPrice: 0, id: 'carne', img: 'carne.png', modifaible: false, title: 'Carne'},
+          {
+            extraPrice: 0,
+            id: 'salsa-bigmac',
+            img: 'salsa-bic-mac.png',
+            modifaible: true,
+            title: 'Salsa Big Mac',
+          },
+        ],
+        title: 'Big Mac',
+      },
+    ],
+  },
+};
+
+const drinks: Record<string, ProductType> = {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  'fanta-chica': {
+    id: 'fanta-chica',
+    img: 'Bebida.png',
+    title: 'Fanta Chica',
+    description:
+      'Burbujas contra el calor, contra la sed, contra el aburrimiento. ' +
+      'Si tus comidas llevan gaseosa, están listas para enfrentar lo que sea, ' +
+      'elegila en tamaño regular, mediano o grande.',
+    ingredients: undefined,
+    categoryId: 'drinks',
+  },
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  'coca-cola-zero-grande': {
+    categoryId: 'drinks',
+    id: 'coca-cola-zero-grande',
+    img: 'Bebida.png',
+    title: 'Coca Cola Zero Grande',
+    description:
+      'Burbujas contra el calor, contra la sed, contra el aburrimiento. Si tus comidas llevan ' +
+      'gaseosa, están listas para enfrentar lo que sea, elegila en tamaño regular, mediano o ' +
+      'grande.',
+  },
+};
 
 describe('[MenuBuilder]', () => {
-  it('should create a new MenuBuilder instance with an empty menu', () => {
-    const menuBuilder = new MenuBuilder();
+  describe('Test `MenuBuilder.constructor`', () => {
+    it('creates a empty instance menu', () => {
+      const menuBuilder = new MenuBuilder();
 
-    expect(menuBuilder).toBeInstanceOf(MenuBuilder);
-    expect(menuBuilder.getMenu()).toEqual({id: '', image: '', name: '', price: 0, products: []});
+      expect(menuBuilder).toBeInstanceOf(MenuBuilder);
+      expect(menuBuilder.getMenu()).toEqual({id: '', image: '', name: '', price: 0, products: []});
+    });
   });
 
-  it('should build a main menu', async () => {
-    const menuBuilder = new MenuBuilder();
+  describe('Test `MenuBuilder.withMainMenu` method', () => {
+    it('returns a MenuBuilder promise', async () => {
+      const menuBuilder = new MenuBuilder();
+      const menuId = 'kj7Stiwpn5';
+      expect(await menuBuilder.withMainMenu(menuId)).toBeInstanceOf(MenuBuilder);
+    });
 
-    expect((await menuBuilder.withMainMenu('kj7Stiwpn5')).getMenu()).toEqual({
-      id: 'kj7Stiwpn5',
-      image: 'McCOMBOBIGMACGrande.png',
-      name: 'McCombo BIG MAC Grande',
-      price: 990,
-      products: [
-        {
-          categoryId: 'burgers',
-          description: 'La hamburguesa más famosa del mundo. Un sabor único.',
-          id: 'big_mac',
-          img: 'big_mac.png',
-          ingredients: [
-            {
-              extraPrice: 0,
-              id: 'pan-arriba',
-              img: 'Pan+arriba.png',
-              modifaible: false,
-              title: 'Pan',
-            },
-            {extraPrice: 0, id: 'pan-abajo', img: 'Pan+abajo.png', modifaible: false, title: 'Pan'},
-            {extraPrice: 0, id: 'carne', img: 'carne.png', modifaible: false, title: 'Carne'},
-            {
-              extraPrice: 0,
-              id: 'salsa-bigmac',
-              img: 'salsa-bic-mac.png',
-              modifaible: true,
-              title: 'Salsa Big Mac',
-            },
-          ],
-          title: 'Big Mac',
-        },
-      ],
+    it('should build a main menu', async () => {
+      const menuBuilder = new MenuBuilder();
+      const menuId = 'kj7Stiwpn5';
+
+      expect((await menuBuilder.withMainMenu(menuId)).getMenu()).toEqual(menus[menuId]);
+    });
+
+    it('checks the main menu is replaced', async () => {
+      const menuBuilder = new MenuBuilder();
+      const menuId1 = 'kj7Stiwpn5';
+      const menuId2 = '4IkmjbhAKy';
+
+      await menuBuilder.withMainMenu(menuId1);
+
+      expect((await menuBuilder.withMainMenu(menuId2)).getMenu()).toEqual(menus[menuId2]);
+    });
+  });
+
+  describe('Test `MenuBuilder.withDrink` function', () => {
+    it('throws an error if the main menu did not create before', async () => {
+      expect.assertions(1);
+
+      try {
+        await new MenuBuilder().withDrink('fanta-chica');
+      } catch (error) {
+        expect(error).toEqual(new Error(ERRORS.mainMenuNoExist));
+      }
+    });
+
+    it('returns a MenuBuilder promise', async () => {
+      const menuId = 'kj7Stiwpn5';
+      const menuBuilder = new MenuBuilder();
+      await menuBuilder.withMainMenu(menuId);
+
+      expect(await menuBuilder.withDrink('fanta-chica')).toBeInstanceOf(MenuBuilder);
+    });
+
+    it('adds the drink in the product list', async () => {
+      const menuId = 'kj7Stiwpn5';
+      const drinkdId = 'fanta-chica';
+      const menu = {...menus[menuId], products: [...menus[menuId].products]};
+      const menuBuilder = new MenuBuilder();
+
+      menu.products.push(drinks[drinkdId]);
+
+      await menuBuilder.withMainMenu(menuId);
+      await menuBuilder.withDrink(drinkdId);
+
+      expect(menuBuilder.getMenu()).toEqual(menu);
+    });
+
+    it('throws when the drink id is not exist', async () => {
+      const menuId = 'kj7Stiwpn5';
+      const drinkId = 'invalid-id';
+      const menuBuilder = new MenuBuilder();
+
+      expect.assertions(1);
+
+      try {
+        await menuBuilder.withMainMenu(menuId);
+        await menuBuilder.withDrink(drinkId);
+      } catch (error) {
+        expect(error).toEqual(new Error(ERRORS.drinkIdNotfound(drinkId)));
+      }
+    });
+
+    it('replaces the new drink by the current drink in the menu', async () => {
+      const menuId = 'kj7Stiwpn5';
+      const drinkdId1 = 'fanta-chica';
+      const drinkdId2 = 'coca-cola-zero-grande';
+      const menu = {...menus[menuId], products: [...menus[menuId].products]};
+      const menuBuilder = new MenuBuilder();
+
+      menu.products.push(drinks[drinkdId2]);
+
+      await menuBuilder.withMainMenu(menuId);
+      await menuBuilder.withDrink(drinkdId1);
+      await menuBuilder.withDrink(drinkdId2);
+
+      expect(menuBuilder.getMenu()).toEqual(menu);
     });
   });
 });
--- src/api/products/builder.ts
+++ src/api/products/builder.ts
@@ -1,34 +1,79 @@
 import type {ComboId, ComboType} from 'src/@types/combos';
-import type {MenuType} from 'src/@types/product.d';
+import type {MenuType, ProductType} from 'src/@types/product.d';
 import {getComboDetailByIdFromApi} from './combosApi';
+import {getProductsByCategoryFromApi} from './productsApi';
+
+export const ERRORS = {
+  mainMenuNoExist: 'The main menu has not been added',
+  drinkIdNotfound: (id: string) => `The drink '${id}' does not exist`,
+};
 
 export type MenuBuilderInterface = {
-  withMainMenu(id: ComboId): void;
+  withMainMenu(id: ComboId): Promise<MenuBuilder>;
+  withDrink(id: string): Promise<MenuBuilder>;
   // WithMainComplement(id: string): void;
-  // withDrink(id: string): void;
-  getMenu(): MenuType | undefined;
+  getMenu(): MenuType;
   reset(): void;
 };
 
 export class MenuBuilder implements MenuBuilderInterface {
   #menu: MenuType;
+  #existMainMenu: boolean;
 
   constructor() {
-    this.#menu = this.buildMenuPlaceholder();
+    this.#menu = undefined as unknown as MenuType;
+    this.#existMainMenu = false;
+
+    this.reset();
   }
 
   reset(): void {
     this.#menu = this.buildMenuPlaceholder();
+    this.#existMainMenu = false;
   }
 
   async withMainMenu(id: ComboId): Promise<MenuBuilder> {
-    const combo: ComboType = await getComboDetailByIdFromApi(id);
-    this.buildMenuMinimalInfo(combo);
+    this.reset();
+    this.buildMenuMinimalInfo(await getComboDetailByIdFromApi(id));
+    this.#existMainMenu = true;
 
     return this;
   }
 
-  getMenu(): MenuType | undefined {
+  /**
+   * Add the `drinkId` drink in the menu.
+   *
+   * Notes:
+   * - To add the drink, first, the instance must create the main menu.
+   * - Only one drink: If the instance adds other drink is added, it deletes the previous.
+   *
+   * @param drinkId Drink id.
+   * @return Promise of the current instance.
+   * @throws Error The instance doesn't has the main manu.
+   * @throws The `drinkId` drink doesn't exist.
+   */
+  async withDrink(drinkId: string): Promise<MenuBuilder> {
+    if (!this.#existMainMenu) {
+      throw new Error(ERRORS.mainMenuNoExist);
+    }
+
+    const allDrinks = await getProductsByCategoryFromApi('drinks');
+    const drink = allDrinks.items.find(({id}) => id === drinkId);
+
+    if (drink === undefined) {
+      throw new Error(ERRORS.drinkIdNotfound(drinkId));
+    }
+
+    this.#menu.products = this.#menu.products.filter((product) => product.categoryId !== 'drinks');
+    this.#menu.products.push(drink);
+
+    return this;
+  }
+
+  /**
+   * Get the menu created by the class.
+   */
+  getMenu(): MenuType {
     const result = this.#menu;
     this.reset();
     return result;

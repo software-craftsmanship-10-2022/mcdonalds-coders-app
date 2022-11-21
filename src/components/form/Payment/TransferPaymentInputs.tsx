d517921e29e64a53962c350a521e92125fabb4fe
jmora-dev <dam2mora@gmail.com>
2022-11-21 20:43:00 +0100

    Refactor checkout forms

--- src/components/form/Payment/TransferPaymentInputs.tsx
+++ src/components/form/Payment/TransferPaymentInputs.tsx
@@ -1,34 +1,11 @@
 import McInput from '../../input/McInput';
 
-export type TransferPaymentInputsProps = {
-  setFullName: (fullName: string) => void;
-  setIBAN: (iban: string) => void;
-};
-
 // Card input info
-const TransferPaymentInputs = ({setFullName, setIBAN}: TransferPaymentInputsProps) => {
+const TransferPaymentInputs = () => {
   return (
     <div className="UserFormContainer">
-      <McInput
-        id={'input-name'}
-        type={'text'}
-        label={'Nombre y apellidos'}
-        width={'100%'}
-        value={''}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setFullName(e.target.value);
-        }}
-      />
-      <McInput
-        id={'input-name'}
-        type={'text'}
-        label={'IBAN'}
-        width={'100%'}
-        value={''}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setIBAN(e.target.value);
-        }}
-      />
+      <McInput id="name" type="text" label={'Nombre y apellidos'} width={'100%'} />
+      <McInput id="iban" type="text" label={'IBAN'} width={'100%'} />
     </div>
   );
 };
--- src/components/views/orders/Checkout.tsx
+++ src/components/views/orders/Checkout.tsx
@@ -1,34 +1,26 @@
 // <<<<<<< HEAD
-import {useState} from 'react';
+import React, { useState } from 'react';
 import type Order from 'src/api/orders/Order';
-import McButton from 'src/components/buttons/McButton';
 import DonationOptions from 'src/components/donation/DonationOptions';
-import type {PaymentMethodType} from 'src/components/form/Payment/constants/paymentMethodsTypes';
-import {PAYMENT_METHODS} from 'src/components/form/Payment/constants/paymentMethodsTypes';
-import PaymentMethodForm from 'src/components/form/Payment/PaymentMethodForm';
+import type { PaymentMethodType } from 'src/components/form/payment/constants/paymentMethodsTypes';
+import { PAYMENT_METHODS } from 'src/components/form/payment/constants/paymentMethodsTypes';
+import PaymentMethodForm from 'src/components/form/payment/PaymentMethodForm';
 import InfoModal from 'src/components/modal/InfoModal';
-import {PAYMENT_TYPE} from 'src/config';
-import {useOrderContext} from 'src/context/OrderContext';
+import { PAYMENT_TYPE } from 'src/config';
+import { useOrderContext } from 'src/context/OrderContext';
 import useFormat from 'src/hooks/useFormat';
 import acceptOrder from 'src/Payment/acceptOrder';
 import Card from 'src/Payment/models/Card/Card';
-import {DebitPaymentStrategy} from 'src/Payment/models/Debit/Debit';
+import { DebitPaymentStrategy } from 'src/Payment/models/Debit/Debit';
 import type Payment from 'src/Payment/models/Payment/Payment';
 import OrderDetail from '../../orders/OrderDetail';
-import {
-  useBankInfo,
-  useCardInfo,
-  useDonation,
-  useIsCardValid,
-  usePaymentMethod,
-  usePaymentWarningModal,
-} from './hooks';
+import { useDonation, usePaymentWarningModal } from './hooks';
 
-type CardDetailsType = {
-  number: string;
-  date: string;
-  cvc: string;
-};
+// Type CardDetailsType = {
+//   number: string;
+//   date: string;
+//   cvc: string;
+// };
 
 type DetailProps = {
   order: Order;
@@ -78,35 +70,71 @@ const Checkout = () => {
 
   const onSelectPaymentMethod = (methodId: string) => {
     const method = PAYMENT_METHODS.find((method) => method.id === methodId);
+    setSelectedMethod(method);
+  };
+
+  const handlePaymentSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    selectedMethod?.handleForm(event);
   };
 
   return (
-    <div className="Detail">
-      <div className="detail-box">
-        <OrderDetail order={order} />
-        <PaymentMethodForm
-          selectedPaymentMethodId={paymentMethod}
-          onSelectedMethod={updatePaymentMethod}
-        />
-        {/* <PaymentForm
-          defaultPaymentMethod={paymentMethod}
-          handleSelectedMethod={updatePaymentMethod}
-        /> */}
-        {/* {paymentMethod === PAYMENT_TYPE.debit && (
-          <PaymentInputs
-            setCardCVC={cardUpdate.cvc}
-            setCardDate={cardUpdate.date}
-            setCardNumber={cardUpdate.number}
-            setCardIsValid={updateCardValidity}
+    <form onSubmit={handlePaymentSubmit}>
+      <div className="Detail">
+        <div className="detail-box">
+          <OrderDetail order={order} />
+          <PaymentMethodForm
+            selectedPaymentMethodId={selectedMethod ? selectedMethod.id : ''}
+            onSelectedMethod={onSelectPaymentMethod}
           />
-        )}
-        {paymentMethod === PAYMENT_TYPE.transfer && (
-          <TransferInputs setFullName={bankUpdate.fullName} setIBAN={bankUpdate.iban} />
-        )} */}
-        <DonationOptions
-          formDonationIsVisible={formDonationIsVisible}
-          updateDonationFormVisibility={updateDonationFormVisibility}
-          updateDonationValue={updateDonationValue}
+
+          {selectedMethod?.formComponent()}
+
+          {/* <button type="submit">Submit</button> */}
+
+          {/* {paymentMethod === PAYMENT_TYPE.debit && (
+            <PaymentInputs
+              setCardCVC={cardUpdate.cvc}
+              setCardDate={cardUpdate.date}
+              setCardNumber={cardUpdate.number}
+              setCardIsValid={updateCardValidity}
+            />
+          )}
+          {paymentMethod === PAYMENT_TYPE.transfer && (
+            <TransferInputs setFullName={bankUpdate.fullName} setIBAN={bankUpdate.iban} />
+          )} */}
+          <DonationOptions
+            formDonationIsVisible={formDonationIsVisible}
+            updateDonationFormVisibility={updateDonationFormVisibility}
+            updateDonationValue={updateDonationValue}
+          />
+        </div>
+        <div className="detail-total">
+          <p>Total</p>
+          <p>{currencyFormatter().format(order.getTotalPrice() + donationValue)}</p>
+        </div>
+        <button type="submit" className="McButton fixed">
+          Enviar pedido
+        </button>
+        {/* <McButton
+          text={'Enviar pedido'}
+          onClick={() => {
+            acceptOrder({
+              confirmOrder,
+              donationValue,
+              operationData,
+              order,
+              paymentMethod,
+              updateCardWarning,
+            });
+          }}
+          fixed
+        /> */}
+        <InfoModal
+          toggle={toggleWarningModalVisibility}
+          isOpen={warningModalIsVisible}
+          title="Atención"
+          message={modalWarningMessage}
         />
       </div>
       <div className="detail-total">

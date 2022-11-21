94fb0647745fef708ddfb4bae1b55655621baec2
jocalu <jocalu87@gmail.com>
2022-11-21 20:12:18 +0100

    refactor checkoutSwitcher

--- src/components/views/orders/CheckoutSwitcher.tsx
+++ src/components/views/orders/CheckoutSwitcher.tsx
@@ -1,23 +1,19 @@
 import {useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
-import saveOrder from 'src/api/orders/saveOrder';
 import {useOrderContext} from 'src/context/OrderContext';
 import useLocalStorage from 'src/hooks/useLocalStorage';
 // Import type Order from 'src/Payment/models/Order/Order';
-import type Order from 'src/api/orders/Order';
-import type Payment from 'src/Payment/models/Payment/Payment';
 import {STORAGE, URLS} from '../../../config';
 import UserForm from '../../form/UserForm';
 import Checkout from './Checkout';
 import './Checkout.css';
-import {useDonation, useIsUserValidated} from './hooks';
+import {useIsUserValidated} from './hooks';
 
 const CheckoutSwitcher = () => {
   const navigate = useNavigate();
   // User validation check
   const {isUserValidated, updateUserValidatedStatus} = useIsUserValidated();
-  const {donationValue} = useDonation();
-  const {order, updateOrder} = useOrderContext();
+  const {order} = useOrderContext();
   const {getStorageItem} = useLocalStorage();
 
   useEffect(() => {
@@ -33,17 +29,10 @@ const CheckoutSwitcher = () => {
     }
   }, [order, navigate, getStorageItem]);
 
-  const confirmOrder = async (payment: Payment, order: Order) => {
-    order.setPayment(payment.getPaymentType());
-    payment.pay();
-    updateOrder(await saveOrder(order));
-    navigate(URLS.ordersCurrent);
-  };
-
   return (
     <div className="Checkout">
       {!isUserValidated && <UserForm setIsValidated={updateUserValidatedStatus} />}
-      {isUserValidated && <Checkout order={order} confirmOrder={confirmOrder} />}
+      {isUserValidated && <Checkout />}
     </div>
   );
 };

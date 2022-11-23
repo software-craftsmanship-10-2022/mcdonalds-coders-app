2ee5244e55f69be5d69f6c4bbbfccc3490a883ee
jocalu <jocalu87@gmail.com>
2022-11-23 20:46:25 +0100

    feature#5 fix: delete accept order <3

--- src/Payment/models/PaymentContext/PaymentContext.test.ts
+++ src/Payment/models/PaymentContext/PaymentContext.test.ts
@@ -3,7 +3,7 @@ import {DebitPaymentStrategy} from '../Debit/Debit';
 import {PaymentContext} from './PaymentContext';
 
 describe('Given a PaymentContext class', () => {
-  const validCard = () => new Card('1299999999999999', '12 / 24', 123);
+  const validCard = () => new Card('1299999999999999', '12 / 24', '123');
   const debitStrategy = new DebitPaymentStrategy(validCard());
   const payment = new PaymentContext(debitStrategy);
 

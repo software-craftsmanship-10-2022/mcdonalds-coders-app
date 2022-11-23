c50ecebe19cf785481686c172713cb7f5e023e82
jmora-dev <dam2mora@gmail.com>
2022-11-23 18:30:43 +0100

    Return paymentStrategy on handle submit

--- src/components/views/orders/Checkout.tsx
+++ src/components/views/orders/Checkout.tsx
@@ -1,34 +1,15 @@
 import React, {useState} from 'react';
-import type Order from 'src/api/orders/Order';
 import DonationOptions from 'src/components/donation/DonationOptions';
 import type {PaymentMethodType} from 'src/components/form/payment/constants/paymentMethodsTypes';
 import {PAYMENT_METHODS} from 'src/components/form/payment/constants/paymentMethodsTypes';
 import PaymentMethodForm from 'src/components/form/payment/PaymentMethodForm';
 import InfoModal from 'src/components/modal/InfoModal';
 import useFormat from 'src/hooks/useFormat';
-import Card from 'src/Payment/models/Card/Card';
-import {DebitPaymentStrategy} from 'src/Payment/models/Debit/Debit';
-import type Payment from 'src/Payment/models/Payment/Payment';
 import OrderDetail from '../../orders/OrderDetail';
 import {useDonation, usePaymentWarningModal} from './hooks';
 
-// Type CardDetailsType = {
-//   number: string;
-//   date: string;
-//   cvc: string;
-// };
-
-type DetailProps = {
-  order: Order;
-  confirmOrder: (payment: Payment, order: Order) => void;
-};
-
 const Checkout = () => {
   const [currencyFormatter] = useFormat();
-  // Const {paymentMethod, updatePaymentMethod} = usePaymentMethod(PAYMENT_TYPE.cash);
-  // const {cardData, cardUpdate} = useCardInfo();
-  // Const {bankData, bankUpdate} = useBankInfo();
-  // const {updateCardValidity} = useIsCardValid();
   const {formDonationIsVisible, donationValue, updateDonationFormVisibility, updateDonationValue} =
     useDonation();
   const {
@@ -39,8 +20,6 @@ const Checkout = () => {
   } = usePaymentWarningModal();
   const [selectedMethod, setSelectedMethod] = useState<PaymentMethodType | undefined>(undefined);
 
-  // Const operationData = paymentMethod === PAYMENT_TYPE.debit ? cardData : bankData;
-
   const onSelectPaymentMethod = (methodId: string) => {
     const method = PAYMENT_METHODS.find((method) => method.id === methodId);
     setSelectedMethod(method);
@@ -48,31 +27,13 @@ const Checkout = () => {
 
   const handlePaymentSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    selectedMethod?.handleForm(event);
+
+    if (!selectedMethod) {
+      throw new Error();
+    }
+
+    const paymentStrategy = selectedMethod?.handleForm(event);
   };
-  /*  Const operationData = paymentMethod === PAYMENT_TYPE.debit ? cardData : bankData; */
-
-  // order.setStatus(OrderStatus.pending);
-  // order.setPayment(payment.getPaymentType());
-  // updateOrder(await saveOrder(order));
-  // payment.pay();
-  // order.setStatus(OrderStatus.preparing);
-  // localStorage.setItem(
-  //   STORAGE.orders,
-  //   JSON.stringify({
-  //     ...order,
-  //     total: order.getTotalPrice() + donationValue,
-  //     paymentType: payment.getPaymentType(),
-  //   }),
-  // );
-  // navigate(URLS.root);
-
-  const {number, date, cvc} = cardData;
-
-  // Cuando tenemos tipo de pag
-  const paymentStrategy = new DebitPaymentStrategy(new Card(number, date, cvc));
-
-  order.setPayment(paymentMethod);
 
   return (
     <form onSubmit={handlePaymentSubmit}>
@@ -86,19 +47,6 @@ const Checkout = () => {
 
           {selectedMethod?.formComponent()}
 
-          {/* <button type="submit">Submit</button> */}
-
-          {/* {paymentMethod === PAYMENT_TYPE.debit && (
-            <PaymentInputs
-              setCardCVC={cardUpdate.cvc}
-              setCardDate={cardUpdate.date}
-              setCardNumber={cardUpdate.number}
-              setCardIsValid={updateCardValidity}
-            />
-          )}
-          {paymentMethod === PAYMENT_TYPE.transfer && (
-            <TransferInputs setFullName={bankUpdate.fullName} setIBAN={bankUpdate.iban} />
-          )} */}
           <DonationOptions
             formDonationIsVisible={formDonationIsVisible}
             updateDonationFormVisibility={updateDonationFormVisibility}
@@ -112,20 +60,6 @@ const Checkout = () => {
         <button type="submit" className="McButton fixed">
           Enviar pedido
         </button>
-        {/* <McButton
-          text={'Enviar pedido'}
-          onClick={() => {
-            acceptOrder({
-              confirmOrder,
-              donationValue,
-              operationData,
-              order,
-              paymentMethod,
-              updateCardWarning,
-            });
-          }}
-          fixed
-        /> */}
         <InfoModal
           toggle={toggleWarningModalVisibility}
           isOpen={warningModalIsVisible}

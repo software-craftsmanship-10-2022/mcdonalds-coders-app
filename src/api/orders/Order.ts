a5282482bde1c92ccf32e38fd1894a86e6418b61
jocalu <jocalu87@gmail.com>
2022-11-24 10:28:39 +0100

    refactor: remove optional chainings

--- src/api/orders/Order.ts
+++ src/api/orders/Order.ts
@@ -37,7 +37,7 @@ export default class Order {
    * Check if the `order.items` list is empty.
    */
   isItemsEmpty(): boolean {
-    return this.order.items?.length === 0;
+    return this.order.items.length === 0;
   }
 
   /**
@@ -46,7 +46,7 @@ export default class Order {
    * @param item new item.
    */
   addItem(item: MenuType): void {
-    this.order.items?.push(item);
+    this.order.items.push(item);
   }
 
   /**
@@ -104,7 +104,7 @@ export default class Order {
    * Get the price total of the items in the `items` list.
    */
   getTotalPrice(): number {
-    return this.order.items?.reduce((total: number, {price}) => total + price, 0);
+    return this.order.items.reduce((total: number, {price}) => total + price, 0);
   }
 
   /**

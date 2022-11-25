61bc1c945e604b92e5d43f779605b04e73eb0bf8
jmora-dev <dam2mora@gmail.com>
2022-11-25 18:05:12 +0100

    feature#42/Add voucher data and api types

--- src/@types/payments.d.ts
+++ src/@types/payments.d.ts
@@ -9,8 +9,21 @@ enum PaymentMethods {
   paypal,
 }
 
-interface VoucherStrategy {
+export interface VoucherStrategy {
   discount(amount: number);
 }
+
+export enum VoucherTypes {
+  percentage,
+  total,
+  fixed,
+}
+
+export type VoucherApiType = {
+  code: string;
+  type: VoucherTypes;
+  discount: number;
+  expirationDate: Date;
+};
 
 export {type PaymentStrategy, PaymentMethods};
---
+++ src/data/voucher.ts
@@ -0,0 +1,29 @@
+import type {VoucherApiType} from 'src/@types/payments';
+import {VoucherTypes} from 'src/@types/payments';
+
+export const VOUCHERS: VoucherApiType[] = [
+  {
+    code: 'MENOS30',
+    type: VoucherTypes.fixed,
+    discount: 30,
+    expirationDate: new Date('2030-01-01T00:00:00'),
+  },
+  {
+    code: 'ANTES-MOLABAS',
+    type: VoucherTypes.percentage,
+    discount: 10,
+    expirationDate: new Date('2000-01-01T00:00:00'),
+  },
+  {
+    code: 'BOOM',
+    type: VoucherTypes.total,
+    discount: 0,
+    expirationDate: new Date('2030-01-01T00:00:00'),
+  },
+  {
+    code: '10%',
+    type: VoucherTypes.percentage,
+    discount: 0,
+    expirationDate: new Date('2030-01-01T00:00:00'),
+  },
+];
